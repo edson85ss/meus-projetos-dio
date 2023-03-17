18bd6084bba18299e5ab02a40aecfda9aabd3115
edson85ss <106289609+edson85ss@users.noreply.github.com>
2023-03-17 16:11:06 -0300

    Add files via upload

---
+++ 1-iac.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Desafio de projeto bootcamp DIO / AWS
+
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+useradd carlos -c "Carlos Silva" -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_ADM
+useradd maria -c "Maria Silva" -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_ADM
+useradd joao -c "João Silva" -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_ADM
+
+useradd debora -c "Débora Silva" -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_VEN
+useradd sebastiana -c "Sebastiana Silva" -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_VEN
+useradd roberto -c "Roberto Silva" -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_VEN
+
+useradd josefina -c "Josefina Silva" -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_SEC
+useradd amanda -c "Amanda Silva" -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_SEC
+useradd rogerio -c "Rogerio Silva" -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_SEC
+
+chown root:GRP_ADM /adm/
+chown root:GRP_VEN /ven/
+chown root:GRP_SEC /sec/
+
+chmod 777 /publico
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
